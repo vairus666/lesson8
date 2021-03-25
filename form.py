f46dad331d9bd35a7beb899f577243fd619e4867
vairus666 <43839255+vairus666@users.noreply.github.com>
2021-03-25 11:43:01 +0300

    Update form.py

--- form.py
+++ form.py
@@ -1,42 +1,50 @@
 import math
 
 class figure:
-    a = b = c = 1
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
-        print('Шалом')
-        
 
-    def area(self, a , b, c = 0):
-        if self.form == 'triangle':
-            p = (a+b+c)/2
-            self.area_f = math.sqrt(p * (p-a) * (p-b) * (p-c))
-            print(f"Площадь треугольника равна = {self.square_f}")
-        elif self.form == 'square':
-            self.area_f = a*b
-            print(f"Площадь прямоугольника равна = {self.square_f}")
-        elif self.form == 'ring':
-            self.area_f = math.pi*(a**2)
-            print(f"Площадь прямоугольника равна = {self.square_f}")
+    def area_tr(self):
+        a = float(input('Введите сторону а\n'))
+        b = float(input('Введите сторону b\n'))
+        c = float(input('Введите сторону с\n'))
+        p = (a+b+c)/2
+        area = math.sqrt(p * (p-a) * (p-b) * (p-c))
+        print(f"Площадь треугольника равна = {area:.2f}")
 
-    def per(self, a, b, c):
-        if self.form == 'triangle':
-            self.per_f = (a+b+c)
-            print(f"Площадь треугольника равна = {self.per_f}")
-        elif self.form == 'rectangle':
-            self.per_f = (a+b) * 2
-            print(f"Площадь прямоугольника равна = {self.per_f}")
-        elif self.form == 'ring':
-            self.per_f = (math.pi*a)/2
-            print(f"Площадь прямоугольника равна = {self.per_f}")
+    def area_qa(self):
+        a = float(input('Введите диагональ а\n'))
+        b = float(input('Введите диагональ b\n'))
+        c = float(input('Введите угол между диагоналями\n'))
+        area = (a*b)/2*math.sin(math.radians(c))
+        print(f"Площадь четыреугольника равна = {area:.2f}")
 
-    
+    def area_ri(self):
+        r = float(input('Введите радиус\n'))
+        area = math.pi*(r**2)
+        print(f"Площадь круга равна = {area:.2f}")
 
+    def per_tr(self):
+        a = float(input('Введите сторону а\n'))
+        b = float(input('Введите сторону b\n'))
+        c = float(input('Введите сторону с\n'))
+        per = (a+b+c)
+        print(f"Площадь треугольника равна = {per:.2f}")
 
+    def per_qa(self):
+        a = float(input('Введите сторону а\n'))
+        b = float(input('Введите сторону b\n'))
+        c = float(input('Введите сторону с\n'))
+        d = float(input('Введите сторону d\n'))
+        per = (a+b+c+d)
+        print(f"Площадь четырехугольника равна = {per:.2f}")    
 
+    def per_ri(self):
+        r = float(input('Введите радиус\n'))
+        per = 2*math.pi*r
+        print(f"Площадь круга равна = {per:.2f}")       
 
-
+    def volume_ri(self):
+        r = float(input('Введите радиус\n'))
+        vol = (4*math.pi*r**3)/3
+        print(f"Объем шара равен = {vol:.2f}")
 
 
